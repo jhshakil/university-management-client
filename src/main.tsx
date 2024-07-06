0283ff59dcb2e02dd2ac987f3bef0c45dd334116
jhshakil <shakil11271@gmail.com>
2024-07-06 16:54:26 +0600

    persist redux state

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import routes from "./routes/routes";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { persistor, store } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={routes} />
+      <PersistGate loading={null} persistor={persistor}>
+        <RouterProvider router={routes} />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
